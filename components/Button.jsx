44021e06492b8762b03b3da254679c58fb9f813b
TRISHAN00 <trishansaha43@gmail.com>
2025-07-31 16:41:24 +0600

    create another branch name -v2

--- components/Button.jsx
+++ components/Button.jsx
@@ -1,9 +1,9 @@
-'use client';
-import { Black, hover, Transition } from '@/styles/globalStyleVars';
-import Image from 'next/image';
-import Link from 'next/link';
-import { FiArrowRight } from 'react-icons/fi';
-import styled from 'styled-components';
+"use client";
+import { Black, hover, Transition } from "@/styles/globalStyleVars";
+import Image from "next/image";
+import Link from "next/link";
+import { FiArrowRight } from "react-icons/fi";
+import styled from "styled-components";
 
 const Button = ({
   onClick,
@@ -28,12 +28,18 @@ const Button = ({
   iconColor,
   hoverIconColor,
   marginSm,
-  className
+  className,
 }) => {
   const renderIcon = () => {
     if (!icon) return <FiArrowRight className="btn-icon" size={16} />;
-    return typeof icon === 'string' ? (
-      <Image src={icon} alt="arrow" width={16} height={11.41} className="btn-icon" />
+    return typeof icon === "string" ? (
+      <Image
+        src={icon}
+        alt="arrow"
+        width={16}
+        height={11.41}
+        className="btn-icon"
+      />
     ) : (
       icon
     );
@@ -47,36 +53,68 @@ const Button = ({
 
   return (
     <StyledBtn
-      onClick={onClick}
-      className={`${className || ''} dc-btn fade-up`}
-      fontSize={fontSize}
-      fontWeight={fontWeight}
-      color={color}
-      background={background}
-      lineHeight={lineHeight}
-      letterSpacing={letterSpacing}
+      className={`${className || ""} dc-btn fade-up`}
       margin={margin}
-      border={border}
-      borderRadius={borderRadius}
       width={width}
       height={height}
-      hoverBackground={hoverBackground}
-      borderColor={borderColor}
-      hoverColor={hoverColor}
-      hoverIconColor={hoverIconColor}
-      iconColor={iconColor}
       marginSm={marginSm}
     >
       {src ? (
-        src.startsWith('http') || src.startsWith('www') ? (
-          <a href={src} target="_blank" rel="noopener noreferrer">
+        src.startsWith("http") || src.startsWith("www") ? (
+          <a
+            href={src}
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{
+              fontSize,
+              fontWeight,
+              color,
+              backgroundColor: background,
+              lineHeight,
+              letterSpacing,
+              borderRadius,
+              border,
+              borderColor,
+            }}
+          >
             {renderButtonContent()}
           </a>
         ) : (
-          <Link href={src}>{renderButtonContent()}</Link>
+          <Link href={src}>
+            <a
+              style={{
+                fontSize,
+                fontWeight,
+                color,
+                backgroundColor: background,
+                lineHeight,
+                letterSpacing,
+                borderRadius,
+                border,
+                borderColor,
+              }}
+            >
+              {renderButtonContent()}
+            </a>
+          </Link>
         )
       ) : (
-        <a target={target || '_self'}>{renderButtonContent()}</a>
+        <a
+          target={target || "_self"}
+          style={{
+            fontSize,
+            fontWeight,
+            color,
+            backgroundColor: background,
+            lineHeight,
+            letterSpacing,
+            borderRadius,
+            border,
+            borderColor,
+          }}
+        >
+          {renderButtonContent()}
+        </a>
       )}
     </StyledBtn>
   );
@@ -84,9 +122,9 @@ const Button = ({
 
 const StyledBtn = styled.div`
   &.dc-btn {
-    margin: ${(props) => props.margin || '0'};
-    width: ${(props) => props.width || 'fit-content'};
-    height: ${(props) => props.height || '44'}px;
+    margin: ${(props) => props.margin || "0"};
+    width: ${(props) => props.width || "fit-content"};
+    height: ${(props) => props.height || "44"}px;
     cursor: pointer;
 
     a {
@@ -95,20 +133,21 @@ const StyledBtn = styled.div`
       height: 100%;
       align-items: center;
       justify-content: center;
-      font-size: ${(props) => props.fontSize || '16'}px;
+      font-size: ${(props) => props.fontSize || "16"}px;
       font-weight: ${(props) => props.fontWeight || 500};
       margin: 0;
-      line-height: ${(props) => props.lineHeight || '20'}px;
+      line-height: ${(props) => props.lineHeight || "20"}px;
       background-color: ${(props) => props.background || `#FFF`};
       position: relative;
-      border-radius: ${(props) => props.borderRadius || '22'}px;
+      border-radius: ${(props) => props.borderRadius || "22"}px;
       overflow: hidden;
       z-index: 0;
       transition: border 0.3s ease;
       padding: 12px 36px;
       box-sizing: border-box;
-      border: ${(p) => p.border || '0'};
-      border-color: ${(props) => props.borderColor || props.hoverBackground || hover};
+      border: ${(p) => p.border || "0"};
+      border-color: ${(props) =>
+        props.borderColor || props.hoverBackground || hover};
       color: ${(props) => props.color || `${Black}`};
 
       span {
@@ -127,7 +166,7 @@ const StyledBtn = styled.div`
         img.btn-icon {
           transition: 0.3s ease;
           filter: ${(props) =>
-            props.iconColor ? `brightness(0) saturate(100%)` : 'none'};
+            props.iconColor ? `brightness(0) saturate(100%)` : "none"};
         }
       }
 
@@ -151,14 +190,15 @@ const StyledBtn = styled.div`
           color: ${(props) => props.hoverColor || `#FFF`};
 
           .btn-icon {
-            color: ${(props) => props.hoverIconColor || props.iconColor || `#FFF`};
+            color: ${(props) =>
+              props.hoverIconColor || props.iconColor || `#FFF`};
           }
 
           img.btn-icon {
             filter: ${(props) =>
               props.hoverIconColor
                 ? `brightness(0) saturate(100%) invert(100%) sepia(0%) hue-rotate(0deg)`
-                : 'none'};
+                : "none"};
           }
         }
 
@@ -175,7 +215,7 @@ const StyledBtn = styled.div`
     }
 
     @media (max-width: 600px) {
-      ${(p) => (p.marginSm ? `margin:${p.marginSm}` : '')}
+      ${(p) => (p.marginSm ? `margin:${p.marginSm}` : "")}
     }
   }
 `;
--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    compiler: {
-        styledComponents: true,
-    },
-    reactStrictMode: false,
-    images: {
-        domains: ['luminoushousingltd.com', 'localhost', 'zoraithost.com'],
-    },
-    transpilePackages: ['swiper', 'lucide-react']
+  compiler: {
+    styledComponents: true,
+  },
+  reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "", // optional, if you're not using a specific port
+        pathname: "/lum-dashboard/admin/uploads/**", // allows anything under that path
+      },
+      {
+        protocol: "https",
+        hostname: "example.com",
+      },
+    ],
+  },
+  transpilePackages: ["swiper", "lucide-react"],
 };
 
 export default nextConfig;
